7c47ea330e8dc0919d284e65a448951c29010719
Ethan <ethan.steip@gmail.com>
2023-09-12 21:36:20 -0400

    finishing sign in page, working on sign up page

--- screens/auth/SignIn.jsx
+++ screens/auth/SignIn.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import {
   Alert,
-  StyleSheet,
+  ImageBackground,
   TextInput,
   SafeAreaView,
   View,
@@ -13,38 +13,38 @@ import { supabase } from "../../lib/supabase";
 import GoogleIcon from "../../icons/google";
 import Facebook from "../../icons/facebook";
 import Apple from "../../icons/apple";
-import SoccerPlayer from "../../assets/illustrations/soccer-player";
-import Email from "../../icons/email";
-import LockClosed from "../../icons/lock-closed";
-import LockOpen from "../../icons/lock-closed";
+import Loading from "../../icons/loading";
 
 export default function SignIn({ navigation }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
-  const [emailIconColour, setEmailIconColour] = useState("#c2c3c4");
-  const [lockIconColour, setLockIconColour] = useState("#c2c3c4");
-  const [secureEntry, setSecureEntry] = useState(true);
 
   async function signInWithEmail() {
     setLoading(true);
+
     const { error } = await supabase.auth.signInWithPassword({
       email: email,
       password: password,
     });
-    console.log("logging In");
 
-    if (error) Alert.alert(error.message);
+    if (error) {
+      Alert.alert(error.message);
+      console.log("Error: ", error);
+    }
+
     setLoading(false);
-
-    //navigation.navigate("Account");
   }
 
   return (
     <SafeAreaView className="flex-1 flex-col justify-end">
       {/* Sign-in Image */}
       <View className="flex-1 bg-white items-center justify-center">
-        <SoccerPlayer />
+        <ImageBackground
+          source={require("roster/screens/illustrations/hockey-scene.png")}
+          resizeMode="cover"
+          style={{ width: 400, height: 330, flex: 1 }}
+        />
       </View>
       {/* Sign-in Form */}
       <View className="flex flex-col p-8 space-y-4 bg-white">
@@ -52,43 +52,26 @@ export default function SignIn({ navigation }) {
           <Text className="text-2xl font-bold text-indigo-900">Sign into Roster</Text>
           <Text className="text-sm text-gray-500">Your Recreational Sports Hub</Text>
         </View>
-        <View className="w-full flex flex-row items-center justify-between border-2 border-gray-400 focus:border-indigo-900 rounded-lg px-4 py-3">
-          <TextInput
-            placeholder="Email Address"
-            className="text-lg"
-            value={email}
-            onChangeText={(text) => setEmail(text)}
-            autoCapitalize={"none"}
-            onFocus={() => setEmailIconColour("#362F78")}
-            onBlur={() => setEmailIconColour("#9CA3AF")}
-          />
-          <Email currentColour={emailIconColour} />
-        </View>
-        <View className="w-full flex flex-row items-center justify-between border-2 border-gray-400 focus:border-indigo-900 rounded-lg px-4 py-3">
-          <TextInput
-            placeholder="Password"
-            className="text-lg"
-            value={password}
-            onChangeText={(text) => setPassword(text)}
-            autoCapitalize={"none"}
-            secureTextEntry={secureEntry}
-            onFocus={() => setLockIconColour("#362F78")}
-            onBlur={() => setLockIconColour("#9CA3AF")}
-          />
-          <TouchableOpacity onPress={() => setSecureEntry(!secureEntry)}>
-            {/* {secureEntry ? (
-              <LockClosed currentColour={lockIconColour} />
-            ) : (
-              <LockOpen currentColour={lockIconColour} />
-            )} */}
-            <LockOpen currentColour={lockIconColour} />
-          </TouchableOpacity>
-        </View>
+        <TextInput
+          placeholder="Email Address"
+          className="bg-gray-50 h-12 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-900 focus:border-indigo-900 block w-full p-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+          autoCapitalize={"none"}
+        />
+        <TextInput
+          placeholder="Password"
+          className="bg-gray-50 h-12 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-900 focus:border-indigo-900 block w-full p-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+          autoCapitalize={"none"}
+          secureTextEntry={true}
+        />
         <TouchableOpacity
           className="flex items-center justify-center p-4 bg-indigo-900 rounded-lg"
           disabled={loading}
           onPress={() => signInWithEmail()}>
-          <Text className="text-white font-semibold">Sign In</Text>
+          <Text className="text-white font-semibold">{loading ? <Loading /> : "Sign In"}</Text>
         </TouchableOpacity>
         <TouchableOpacity
           className="flex items-center justify-center p-3"
@@ -116,22 +99,3 @@ export default function SignIn({ navigation }) {
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    marginTop: 40,
-    padding: 12,
-  },
-  verticallySpaced: {
-    paddingTop: 4,
-    paddingBottom: 4,
-    alignSelf: "stretch",
-  },
-  mt20: {
-    marginTop: 20,
-  },
-  signInContainer: {
-    flexDirection: "row",
-    justifyContent: "center",
-  },
-});
--- screens/auth/SignUp.jsx
+++ screens/auth/SignUp.jsx
@@ -1,74 +1,112 @@
 import React, { useState } from "react";
-import { Alert, StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import {
+  Alert,
+  ImageBackground,
+  SafeAreaView,
+  TextInput,
+  View,
+  Text,
+  TouchableOpacity,
+} from "react-native";
 import { supabase } from "../../lib/supabase";
-import { Button, Input } from "react-native-elements";
+
+import GoogleIcon from "../../icons/google";
+import Apple from "../../icons/apple";
+import Facebook from "../../icons/facebook";
+import Loading from "../../icons/loading";
 
 export default function SignUp({ navigation }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmationPassword, setConfirmationPassword] = useState("");
   const [loading, setLoading] = useState(false);
 
   async function signUpWithEmail() {
     setLoading(true);
-    const { error } = await supabase.auth.signUp({
-      email: email,
-      password: password,
-    });
 
-    if (error) Alert.alert(error.message);
-    setLoading(false);
+    // check that passwords match
+    if (password === confirmationPassword) {
+      const { error } = await supabase.auth.signUp({
+        email: email,
+        password: password,
+      });
+
+      if (error) Alert.alert(error.message);
+      setLoading(false);
+    } else {
+      Alert.alert("Passwords do not match - please try again");
+      setLoading(false);
+    }
   }
 
   return (
-    <View style={styles.container}>
-      <View style={[styles.verticallySpaced, styles.mt20]}>
-        <Input
-          label="Email"
-          leftIcon={{ type: "font-awesome", name: "envelope" }}
-          onChangeText={(text) => setEmail(text)}
+    <SafeAreaView className="flex-1 flex-col justify-end">
+      {/* Sign-in Image */}
+      <View className="flex-1 bg-white items-center justify-center">
+        <ImageBackground
+          source={require("roster/screens/illustrations/volleyball-scene.png")}
+          resizeMode="cover"
+          style={{ width: 400, height: 330, flex: 1 }}
+        />
+      </View>
+      {/* Sign-in Form */}
+      <View className="flex flex-col p-8 space-y-4 bg-white">
+        <View className="w-full flex flex-col">
+          <Text className="text-2xl font-bold text-indigo-900">Sign Up For Roster</Text>
+          <Text className="text-sm text-gray-500">Your Recreational Sports Hub</Text>
+        </View>
+        <TextInput
+          placeholder="Email Address"
+          className="bg-gray-50 h-12 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-900 focus:border-indigo-900 block w-full p-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           value={email}
-          placeholder="email@address.com"
+          onChangeText={(text) => setEmail(text)}
           autoCapitalize={"none"}
         />
-      </View>
-      <View style={styles.verticallySpaced}>
-        <Input
-          label="Password"
-          leftIcon={{ type: "font-awesome", name: "lock" }}
-          onChangeText={(text) => setPassword(text)}
-          value={password}
-          secureTextEntry={true}
+        <TextInput
           placeholder="Password"
+          className="bg-gray-50 h-12 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-900 focus:border-indigo-900 block w-full p-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
           autoCapitalize={"none"}
+          secureTextEntry={true}
         />
-      </View>
-      <View style={styles.verticallySpaced}>
-        <Button title="Sign up" disabled={loading} onPress={() => signUpWithEmail()} />
-      </View>
-      <View style={[styles.signInContainer, styles.mt20]}>
-        <TouchableOpacity onPress={() => navigation.navigate("SignIn")}>
-          <Text>Already have an account? Sign In</Text>
+        <TextInput
+          placeholder="Confirm Password"
+          className="bg-gray-50 h-12 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-900 focus:border-indigo-900 block w-full p-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          value={confirmationPassword}
+          onChangeText={(text) => setConfirmationPassword(text)}
+          autoCapitalize={"none"}
+          secureTextEntry={true}
+        />
+        <TouchableOpacity
+          className="flex items-center justify-center p-4 bg-indigo-900 rounded-lg"
+          disabled={loading}
+          onPress={() => signUpWithEmail()}>
+          <Text className="text-white font-semibold">{loading ? <Loading /> : "Sign Up"}</Text>
         </TouchableOpacity>
+        <TouchableOpacity
+          className="flex items-center justify-center p-3"
+          onPress={() => navigation.navigate("SignIn")}>
+          <Text>
+            Already have an account? <Text className="text-indigo-900 font-semibold">Sign In</Text>
+          </Text>
+        </TouchableOpacity>
+        <View className="w-full border-b border-gray-300"></View>
+        <View className="flex items-center justify-center w-full">
+          <Text>Or continue with</Text>
+        </View>
+        <View className="flex flex-row w-auto items-center justify-center space-x-3">
+          <TouchableOpacity className="flex items-center justify-center border border-gray-400 p-3 rounded-full">
+            <GoogleIcon />
+          </TouchableOpacity>
+          <TouchableOpacity className="flex items-center justify-center border border-gray-400 p-3 rounded-full">
+            <Apple />
+          </TouchableOpacity>
+          <TouchableOpacity className="flex items-center justify-center border border-gray-400 p-3 rounded-full">
+            <Facebook />
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    marginTop: 40,
-    padding: 12,
-  },
-  verticallySpaced: {
-    paddingTop: 4,
-    paddingBottom: 4,
-    alignSelf: "stretch",
-  },
-  mt20: {
-    marginTop: 20,
-  },
-  signInContainer: {
-    flexDirection: "row",
-    justifyContent: "center",
-  },
-});
